dd31b9e657f214f6d18f675fe7f12c5cf4f48bd8
EliPoire <eliaspoire@gmail.com>
2023-12-10 16:22:20 -0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(_name_)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/proyecto"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://Multitude6788:6y22ayv9y6h7yc@Multitude6788.mysql.pythonanywhere-services.com/Multitude6788$proyecto"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -17,22 +16,19 @@ class Event(db.Model):
     nombre = db.Column(db.String(100))
     descripcion = db.Column(db.String(300))
     imagen = db.Column(db.String(400))
-    link = db.Column(db.String(200))
-    fecha_horario = db.Column(db.DateTime)  # Cambiado a DateTime
+    fecha_horario = db.Column(db.String(20)) 
 
-    def __init__(self, nombre, descripcion, imagen, link, fecha_horario):
+    def _init_(self, nombre, descripcion, imagen, fecha_horario):
         self.nombre = nombre
         self.descripcion = descripcion
         self.imagen = imagen
-        self.link = link
-        self.fecha_horario = fecha_horario
-
+        self.fecha_horario = fecha_horario 
 with app.app_context():
     db.create_all()
 
 class EventSchema(ma.Schema):
     class Meta:
-        fields = ("id", "nombre", "descripcion", "imagen", "link", "fecha_horario")
+        fields = ("id", "nombre", "descripcion", "imagen", "fecha_horario")
 
 event_schema = EventSchema()
 events_schema = EventSchema(many=True)
@@ -60,13 +56,9 @@ def create_event():
     nombre = request.json["nombre"]
     descripcion = request.json["descripcion"]
     imagen = request.json["imagen"]
-    link = request.json["link"]
-    fecha_horario_str = request.json["fecha_horario"]
+    fecha_horario = request.json["fecha_horario"]  
 
-    # Parsea la cadena de fecha y hora a un objeto DateTime
-    fecha_horario = datetime.strptime(fecha_horario_str, "%d/%m/%Y %H:%M")
-
-    new_event = Event(nombre, descripcion, imagen, link, fecha_horario)
+    new_event = Event(nombre, descripcion, imagen, fecha_horario)
     db.session.add(new_event)
     db.session.commit()
     return event_schema.jsonify(new_event)
@@ -74,14 +66,17 @@ def create_event():
 @app.route("/events/<id>", methods=["PUT"])
 def update_event(id):
     event = Event.query.get(id)
-    event.nombre = request.json["nombre"]
-    event.descripcion = request.json["descripcion"]
-    event.imagen = request.json["imagen"]
-    event.link = request.json["link"]
-    event.fecha_horario = datetime.strptime(request.json["fecha_horario"], "%d/%m/%Y %H:%M")
 
-    db.session.commit()
-    return event_schema.jsonify(event)
+    if event:
+        event.nombre = request.json.get("nombre", event.nombre)
+        event.descripcion = request.json.get("descripcion", event.descripcion)
+        event.imagen = request.json.get("imagen", event.imagen)
+        event.fecha_horario = request.json.get("fecha_horario", event.fecha_horario)
 
-if __name__ == "__main__":
+        db.session.commit()
+        return event_schema.jsonify(event)
+    else:
+        return jsonify({"message": "Evento no encontrado"}), 404
+
+if _name_ == "_main_":
     app.run(debug=True, port=5000)
